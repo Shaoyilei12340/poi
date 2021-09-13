42e18c5380db2043f1dfefaa3b2b3407bf17f93a
Shaoyilei12340 <77570755+Shaoyilei12340@users.noreply.github.com>
2021-09-14 07:46:26 +0800

    Delete client.py

--- client.py
+++
@@ -1,7 +0,0 @@
-import socket
-sk = socket.socket()           # 创建客户套接字
-sk.connect(('41r6s98972.wicp.vip',33516))    # 尝试连接服务器
-sk.send(b'hello!')
-ret = sk.recv(1024)         # 对话(发送/接收)
-print(ret)
-sk.close()            # 关闭客户套接字
